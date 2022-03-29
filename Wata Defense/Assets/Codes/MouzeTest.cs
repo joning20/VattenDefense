b4d47811e25bfc451f490f8794ebe5117a6b3b93
Poon-Falufri <te20simdah@falufri.se>
2022-03-29 10:12:57 +0200

    TowerPlaceAble Prefabs

--- Wata Defense/Assets/Codes/MouzeTest.cs
+++ Wata Defense/Assets/Codes/MouzeTest.cs
@@ -7,10 +7,15 @@ public class MouzeTest : MonoBehaviour
 {
     public Vector3 MousePos;
     public LayerMask CollisionLayer;
+    public LayerMask NotCollisonLayer;
+    public LayerMask TowerCollider;
 
     [SerializeField] float radiusCheck;
     [SerializeField] Transform MoveObject;
     [SerializeField] SpriteRenderer MoveMaterial;
+    [SerializeField] bool Placeable = false;
+
+    public GameObject TowerPrefab;
 
     void Start()
     {
@@ -23,15 +28,32 @@ public class MouzeTest : MonoBehaviour
 
         MoveObject.position = new Vector3(MousePos.x, MousePos.y, MoveObject.position.z);
 
-        if (Physics2D.OverlapCircle(MoveObject.position, radiusCheck, CollisionLayer))
+        if (Physics2D.OverlapCircle(MoveObject.position, radiusCheck, CollisionLayer) && !Physics2D.OverlapCircle(MoveObject.position, radiusCheck, NotCollisonLayer) && !Physics2D.OverlapCircle(MoveObject.position, radiusCheck, TowerCollider))
         {
             print("Placeable");
             MoveMaterial.color = Color.green;
+            Placeable = true;
         }
 
         else
         {
             MoveMaterial.color = Color.red;
+            Placeable = false;
+        }
+
+        //Placing
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+
+            print("Clicked");
+            Vector3 TowerPos =new Vector3(MousePos.x, MousePos.y, -5);
+
+            if (Placeable)
+            {
+                Instantiate(TowerPrefab, TowerPos, transform.rotation);
+                print("Placed");
+            }
         }
     }
 }
