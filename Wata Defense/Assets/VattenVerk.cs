daf2eb155640113ae13fff48ed5dc10644641c05
Jonathan Ingels <79262955+joning20@users.noreply.github.com>
2022-03-29 10:09:27 +0200

    La till Main menu och losing conditions

--- Wata Defense/Assets/VattenVerk.cs
+++ Wata Defense/Assets/VattenVerk.cs
@@ -1,14 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class VattenVerk : MonoBehaviour
 {
     public int Health;
+    public TextMeshProUGUI healthText;
+    public Canvas losingUI;
 
     void Start()
     {
-        
+        losingUI.enabled = false;
     }
 
     // Update is called once per frame
@@ -19,6 +22,17 @@ public class VattenVerk : MonoBehaviour
 
     public void DmgBuilding(int dmg)
     {
-        Health -= dmg;
+
+        if (Health > 0)
+        {
+            Health -= dmg;
+            healthText.text = "Health: " + Health.ToString();
+        }
+        else if (Health == 0)
+        {
+            losingUI.enabled = true;
+        }   
+
+        
     }
 }
